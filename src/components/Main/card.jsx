8a1f921e3d40a4fff6c7a40d554cedd92bfdfb0c
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2021-10-29 17:26:04 +0200

    Kinda gave up on shared layout and used animatepresence to animate the cards on click

--- src/components/Main/card.jsx
+++ src/components/Main/card.jsx
@@ -1,8 +1,9 @@
 import Tilt from 'react-parallax-tilt';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Box, Heading, Text } from '@chakra-ui/layout';
 import { useColorModeValue } from '@chakra-ui/color-mode';
+import { useState } from "react"
 
 
 const CardContainer = styled(motion.div) `
@@ -61,23 +62,66 @@ const CardContainer = styled(motion.div) `
 
 const Card = ({image, id, title}) => {
     const color = useColorModeValue('#3d3d3d', '#f2f2f2');
-    
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleOpen = () => setIsOpen(!isOpen)
+    const MotionBox = motion(Box)
+    const openAnimation = {
+        open : {
+            y: 0, 
+            opacity: 1,
+            height: "200px",
+            transition: {
+                y: {
+                    ease: "easeOut",
+                    duration: .5
+                },
+                opacity: {
+                    duration: "15s"
+                },
+                height: {
+                    duration: .25
+                }
+
+            }
+        },
+        close: {
+            y: 100,
+            opacity: 0,
+            height: 0,
+            transition: {
+                height: {
+                    duration: .5,
+                }
+            }
+        }
+    }
 
     return (
-        <CardContainer whileHover={{ scale: 1.04 }} whileTap={{ scale: 1 }} transition={{}}>
+        <CardContainer whileHover={{ scale: 1.04 }} whileTap={{ scale: 1 }} transition={{}} onClick={toggleOpen}>
             <Tilt
             className='card'
             tiltMaxAngleX={3}
             tiltMaxAngleY={7}
             glareEnable={true}
+            
             >
                 <img src={image} alt='' />
-                <Box key={id} >
-                    <Heading class='text' color={color}>{title}</Heading>
-                    <Text color={color} class='text'>our desctiption</Text>
-                </Box>
+                <AnimatePresence>
+                    {isOpen && (
+                    <MotionBox
+                        key={id}
+                        variants={openAnimation}
+                        initial={{y: -100, height: 0}}
+                        animate={"open"}
+                        exit={"close"}
+                    
+                        >
+                        <Heading class='text' color={color}>{title}</Heading>
+                        <Text color={color} class='text'>our desctiption</Text>
+                    </MotionBox>)}
+                </AnimatePresence>
             </Tilt>
-    </CardContainer>
+        </CardContainer>
     )
 }
 
