08fda6a41641c35ded36bc63c7a703bee7cf14b1
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-11-03 16:00:36 +0100

    TSparticles and front page animations

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,212 @@ import { Link, NavLink } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import { colors } from '../../app.styled';
 import { StyledHome } from './Home.styled';
+import Particles from 'react-tsparticles';
 
 const Home = () => {
-  return <StyledHome>Home page</StyledHome>;
+  const headlineVariant = {
+    initial: {
+      opacity: 0,
+      width: 0,
+      x: 0,
+      y: 0,
+    },
+
+    animate: {
+      opacity: 1,
+      width: 100,
+      x: 0,
+      y: 0,
+      transition: {
+        type: 'linear',
+        delay: 1,
+        duration: 1,
+      },
+    },
+
+    initialSideline: {
+      height: 50,
+      width: 5,
+      y: 0,
+    },
+    animateSideline: {
+      height: 120,
+      width: 5,
+      y: 35,
+      // position: 'absolute',
+      transition: {
+        type: 'linear',
+        delay: 2,
+        duration: 1,
+      },
+    },
+
+    initial2: {
+      width: 120,
+      y: 50,
+    },
+    // animate2: {
+    //   opacity: 1,
+    //   y: 50,
+    //   transition: {
+    //     // when: 'beforeChildren',
+    //     type: 'linear',
+    //     delay: 2,
+    //     duration: 1,
+    //   },
+    // },
+
+    initialh2: {
+      opacity: 1,
+      x: -200,
+    },
+    animateh2: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: 'linear',
+        delay: 3,
+        duration: 1,
+      },
+    },
+  };
+  return (
+    <StyledHome>
+      <ParticleComponent className='particles' />
+
+      <motion.section
+        initial={{ y: 0, x: -120 }}
+        animate={{ y: -30, x: -120 }}
+        transition={{ type: 'linear', duration: 1, delay: 2 }}
+        className='animation-container'
+      >
+        <motion.div
+          variants={headlineVariant}
+          initial='initial'
+          animate='animate'
+          className='hero-text'
+        >
+          <motion.h2>Healine</motion.h2>
+        </motion.div>
+
+        <motion.div
+          variants={headlineVariant}
+          initial='initialSideline'
+          animate='animateSideline'
+          className='sideline'
+        ></motion.div>
+
+        <motion.div
+          variants={headlineVariant}
+          initial='initial2'
+          // animate='animate2'
+          className='hero-text-2'
+        >
+          <motion.h2
+            variants={headlineVariant}
+            initial='initialh2'
+            animate='animateh2'
+          >
+            Healine2
+          </motion.h2>
+        </motion.div>
+      </motion.section>
+    </StyledHome>
+  );
+};
+
+const ParticleComponent = () => {
+  const particlesInit = (main) => {
+    console.log(main);
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+  return (
+    <Particles
+      id='tsparticles'
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: '#000',
+          },
+        },
+        fpsLimit: 100,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: 'push',
+            },
+            onHover: {
+              enable: false,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#ffffff',
+          },
+          links: {
+            color: '#ffffff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
 };
 
 export default Home;
--- src/components/Home/Home.styled.js
+++ src/components/Home/Home.styled.js
@@ -1,3 +1,51 @@
 import styled from 'styled-components';
 
-export const StyledHome = styled.main``;
+export const StyledHome = styled.main`
+  height: 91vh;
+  position: relative;
+  #tsparticles {
+    position: absolute;
+    width: 100%;
+    z-index: -1;
+  }
+  .animation-container {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* width: 100%; */
+
+    .hero-text {
+      color: white;
+      font-size: 1.5rem;
+      overflow: hidden;
+      /* border-right: 5px solid white; */
+      h2 {
+        white-space: nowrap;
+        text-align: center;
+      }
+    }
+
+    .sideline {
+      background: white;
+      /* position: absolute; */
+    }
+    .hero-text-2 {
+      /* background: #def; */
+      color: white;
+      font-size: 1.5rem;
+      overflow: hidden;
+      /* border-left: 5px solid white; */
+      /* transform: translateX(-4.6%); */
+      /* margin-left: 0.5rem; */
+
+      h2 {
+        white-space: nowrap;
+        text-align: center;
+      }
+    }
+  }
+`;
