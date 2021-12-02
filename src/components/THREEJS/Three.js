56e804584a629d81eefd9964e16a51fca7fdd5e5
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-12-02 12:35:24 +0100

    example of mars layout structure

--- src/components/THREEJS/Three.js
+++ src/components/THREEJS/Three.js
@@ -3,6 +3,32 @@ import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { Box } from '@chakra-ui/layout';
 import { TextureLoader } from 'three';
 import { OrbitControls, Stars } from '@react-three/drei';
+import { colors } from '../../app.styled';
+import styled from 'styled-components';
+
+const NotchedDiv = styled.div`
+  position: absolute;
+  top: 25%;
+  right: 2rem;
+  padding: 2rem;
+  width: 50%;
+  height: 25rem;
+  background-color: ${colors.darkBlue};
+  opacity: 0.7;
+  border: 1px solid ${colors.pink};
+  box-shadow: inset 0px 0px 10px ${colors.pink};
+  clip-path: polygon(
+    0 10%,
+    10% 0,
+    90% 0,
+    100% 10%,
+    100% 90%,
+    90% 100%,
+    10% 100%,
+    0% 90%,
+    0% 10%
+  );
+`;
 
 const Sphere = () => {
   const meshRef = useRef();
@@ -18,7 +44,7 @@ const Sphere = () => {
     <>
       <ambientLight intensity={0.2} />
       <directionalLight position={[-1, 0, 0.8]} />
-      <mesh ref={meshRef} position={[-1.5, 0, 0]}>
+      <mesh ref={meshRef} position={[-2, 0, 0]}>
         <sphereGeometry args={[1, 50, 50]} />
         <meshStandardMaterial map={colorMap} />
       </mesh>
@@ -29,13 +55,31 @@ const Sphere = () => {
 const Three = () => {
   return (
     <>
-      <Box h='100vh' w='100%'>
+      <Box h='50rem' w='100%' position='relative' backgroundColor='#000'>
         <Canvas camera={{ position: [0, 0, 5] }}>
           <Suspense fallback={null}>
             <Stars />
             <Sphere />
           </Suspense>
         </Canvas>
+        <NotchedDiv
+        //   h='50rem'
+        //   w='50%'
+        //   backgroundColor={colors.black}
+        //   position='absolute'
+        //   top='0'
+        //   right='0'
+        //   opacity='0.5'
+        >
+          <Box color='white'>
+            <h2>Mars Facts</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis hic quos quaerat illo obcaecati harum alias repellat,
+              quam minus libero.
+            </p>
+          </Box>
+        </NotchedDiv>
       </Box>
     </>
   );
